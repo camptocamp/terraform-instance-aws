6e84daedb75f57e5d9d49d5a83efc1b8670597a3
Christian Kaenzig <christian.kaenzig@camptocamp.com>
2019-08-13 11:15:59 +0200

    Add support and test for instances without a public IP

--- main.tf
+++ main.tf
@@ -151,7 +151,7 @@ resource "null_resource" "provisioner" {
     type = lookup(var.connection, "type", null)
     user = lookup(var.connection, "user", "terraform")
     password = lookup(var.connection, "password", null)
-    host = lookup(var.connection, "host", coalesce((var.eip ? aws_eip.this[count.index].public_ip : ""), aws_instance.this[count.index].public_ip, compact(aws_instance.this[count.index].ipv6_addresses)...))
+    host = lookup(var.connection, "host", coalesce((var.eip ? aws_eip.this[count.index].public_ip : ""), aws_instance.this[count.index].public_ip, var.public_ip ? "" : aws_instance.this[count.index].private_ip, compact(aws_instance.this[count.index].ipv6_addresses)...))
     port = lookup(var.connection, "port", 22)
     timeout = lookup(var.connection, "timeout", "")
     script_path = lookup(var.connection, "script_path", null)
@@ -210,6 +210,7 @@ module "puppet-node" {
       hostname = aws_instance.this[i].private_dns
       connection = {
         host = coalesce((var.eip ? aws_eip.this[i].public_ip : ""), aws_instance.this[i].public_ip, compact(aws_instance.this[i].ipv6_addresses)...)
+        host = coalesce((var.eip ? aws_eip.this[i].public_ip : ""), aws_instance.this[i].public_ip, var.public_ip ? "" : aws_instance.this[i].private_ip, compact(aws_instance.this[i].ipv6_addresses)...)
       }
     }
   ]
@@ -238,7 +239,7 @@ module "rancher-host" {
       hostname = aws_instance.this[i].private_dns
       agent_ip = aws_instance.this[i].private_ip
       connection = {
-        host = coalesce((var.eip ? aws_eip.this[i].public_ip : ""), aws_instance.this[i].public_ip, compact(aws_instance.this[i].ipv6_addresses)...)
+        host = coalesce((var.eip ? aws_eip.this[i].public_ip : ""), aws_instance.this[i].public_ip, var.public_ip ? "" : aws_instance.this[i].private_ip, compact(aws_instance.this[i].ipv6_addresses)...)
       }
       host_labels = merge(
         var.rancher != null ? var.rancher.host_labels : {},
@@ -247,7 +248,7 @@ module "rancher-host" {
           "io.rancher.host.provider" = "aws"
           "io.rancher.host.region" = var.region
           "io.rancher.host.zone" = aws_instance.this[i].availability_zone
-          "io.rancher.host.external_dns_ip" = coalesce((var.eip ? aws_eip.this[i].public_ip : ""), aws_instance.this[i].public_ip, compact(aws_instance.this[i].ipv6_addresses)...)
+          "io.rancher.host.external_dns_ip" = coalesce((var.eip ? aws_eip.this[i].public_ip : ""), aws_instance.this[i].public_ip, var.public_ip ? "" : aws_instance.this[i].private_ip, compact(aws_instance.this[i].ipv6_addresses)...)
         }
       )
     }
--- tests/main.tf
+++ tests/main.tf
@@ -78,6 +78,54 @@ module "instance" {
   }
 }
 
+module "instance_private" {
+  source         = "../"
+  instance_count = var.instance_count
+  key_pair       = var.key_pair
+
+  security_groups = ["sg-064a964f60b3b4d6f"]
+  subnet_ids      = ["subnet-0ae8b71b5b9926c31"]
+
+  public_ip     = false
+  eip           = false
+
+  ami           = data.aws_ami.ami.id
+  instance_type = "t2.micro"
+  ebs_optimized = false
+  eip           = false
+
+  additional_volumes = [
+    {
+      name        = "docker"
+      type        = "gp2"
+      size        = 10
+      device_name = "/dev/xvdp"
+      mount_path  = "/var/lib/docker"
+      fstype      = "ext4"
+    }
+  ]
+
+  tags = {
+    Name = "terraform-instance-aws testing"
+  }
+
+  puppet = {
+    autosign_psk = data.pass_password.puppet_autosign_psk.data["puppet_autosign_psk"]
+    server       = "puppet.camptocamp.net"
+    caserver     = "puppetca.camptocamp.net"
+    role         = "base"
+    environment  = "staging4"
+  }
+
+  rancher = {
+    environment_id = "1a5"
+    host_labels = {
+      foo = "bar"
+      bar = "baz"
+    }
+  }
+}
+
 ###
 # Acceptance test
 #
@@ -87,6 +135,34 @@ resource "null_resource" "acceptance" {
 
   connection {
     host = coalesce(module.instance.this_instance_public_ipv4[count.index], (length(module.instance.this_instance_public_ipv6[count.index]) > 0 ? module.instance.this_instance_public_ipv6[count.index][0] : ""))
+    type = "ssh"
+    user = "root"
+  }
+
+  provisioner "file" {
+    source      = "script.sh"
+    destination = "/tmp/script.sh"
+  }
+
+  provisioner "file" {
+    source      = "goss.yaml"
+    destination = "/root/goss.yaml"
+  }
+
+  provisioner "remote-exec" {
+    inline = [
+      "chmod +x /tmp/script.sh",
+      "sudo /tmp/script.sh",
+    ]
+  }
+}
+
+resource "null_resource" "acceptance_private" {
+  count      = var.instance_count
+  depends_on = ["module.instance_private"]
+
+  connection {
+    host = module.instance.this_instance_private_ipv4[count.index]
     type = "ssh"
     user = "root"
   }
